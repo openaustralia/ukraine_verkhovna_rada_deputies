b53067c7666f04670280cc6c6928588c3cb580f0
Henare Degan <henare.degan@gmail.com>
2015-09-07 14:38:36 +1000

    Note the basic details required by EveryPolitician

--- scraper.rb
+++ scraper.rb
@@ -9,3 +9,13 @@ agent = Mechanize.new
 index_page = agent.get("http://w1.c1.rada.gov.ua/pls/site2/fetch_mps?skl_id=9")
 
 detail_page_urls = index_page.search(".title").map { |e| e.at(:a).attr(:href) }
+
+# record = {
+#   id:
+#   name:
+#   area:
+#   group:
+#   term:
+#   start_date:
+#   end_date:
+# }

49121327167fed3b6237742b768068bafcb6650c
Henare Degan <henare.degan@gmail.com>
2015-09-07 14:35:49 +1000

    Add the page to scrape

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,9 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# The full list of deputies is available at a link on this page:
+# http://w1.c1.rada.gov.ua/pls/site2/p_deputat_list
+# ...that page fires JavaScript that loads the following URL
+page = agent.get("http://w1.c1.rada.gov.ua/pls/site2/fetch_mps?skl_id=9")

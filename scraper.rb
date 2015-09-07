3492991fab33e5663eb017a6f9ed0a7fcaf23f6d
Henare Degan <henare.degan@gmail.com>
2015-09-07 14:38:18 +1000

    Extract detail page URLs

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,6 @@ agent = Mechanize.new
 # The full list of deputies is available at a link on this page:
 # http://w1.c1.rada.gov.ua/pls/site2/p_deputat_list
 # ...that page fires JavaScript that loads the following URL
-page = agent.get("http://w1.c1.rada.gov.ua/pls/site2/fetch_mps?skl_id=9")
+index_page = agent.get("http://w1.c1.rada.gov.ua/pls/site2/fetch_mps?skl_id=9")
+
+detail_page_urls = index_page.search(".title").map { |e| e.at(:a).attr(:href) }
